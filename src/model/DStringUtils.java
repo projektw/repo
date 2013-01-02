78675c540e1284060a5cd242405c4ee6c62af561
Rafal <rafalrataj@gmail.com>
2013-01-02 09:57:20 +0100

    quic & dirty :)

--- src/model/DStringUtils.java
+++ src/model/DStringUtils.java
@@ -9,10 +9,17 @@ public class DStringUtils {
 	public List<String>  getContentBetweenTags(String content, String tagFrom, String tagTo){
 		
 		List <String> lst = new ArrayList<String>();
-		
 		while(content.contains(tagFrom)){;
 			lst.add(content.substring(content.indexOf(tagFrom)+4, content.indexOf(tagTo)));
 			content = content.substring(content.indexOf(tagTo)+4);
+		
+//			 quick and dirty solution for smiles 
+//			<@Podmiot> cześć ;>
+//			< oso> cześć Podmiot 
+			while(content.indexOf(tagFrom) > content.indexOf(tagTo)){
+			content = content.substring(content.indexOf(tagFrom));
+			}
+			
 		}
 		return lst;
 		
